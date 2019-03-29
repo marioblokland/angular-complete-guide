da8822b11d0585f0cf5798ea24b26b6106adcf37
Mario Blokland <mario@marioblokland.com>
2019-03-29 10:53:54 +0100

    Added some dummy recipe list content

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,11 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Pizza', 'A tasty Pizza', 'https://recipes.timesofindia.com/photo/53110049.cms?imgsize=148092'),
+    new Recipe('Sushi', 'Some tasty Sushi', 'https://recipes.timesofindia.com/thumb/msid-52806533,width-400,resizemode-4/52806533.jpg')
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
-
 }
